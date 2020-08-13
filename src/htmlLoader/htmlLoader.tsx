46230b1e8b8c00ba0c081a766894a3cf082aba91
ntgeek <2584172930@qq.com>
2020-08-13 14:57:07 +0800

    fix:new htmlLoader

--- src/htmlLoader/htmlLoader.tsx
+++ src/htmlLoader/htmlLoader.tsx
@@ -1,36 +1,51 @@
-import { RE_LABEL_STYLE, RE_LABEL_SCRIPT, RE_LABEL_LINK, RE_LABEL_META, RE_LABEL_BODY, RE_LABEL_HEAD } from "./regex"
+import {
+    RE_LABEL_STYLE, RE_LABEL_SCRIPT, RE_LABEL_SCRIPT_CONTENT,
+    RE_LABEL_SCRIPT_URL, RE_LABEL_LINK, RE_LABEL_META, RE_LABEL_BODY
+} from "./regex"
 import { request } from "../util/request"
 import { memorize } from "../util/handlers"
 import { sandbox } from "../sandbox/sandbox"
-//loadHtml 载入html到app定义的容器中
-export async function reHtml(url: string): Promise<string> {
+export async function reHtml(url: string): Promise<[string, Array<string>]> {
     let html = await request(url),
-        body = RE_LABEL_BODY.exec(html),
-        head = RE_LABEL_HEAD.exec(html),
         period,
-        result: string = '';
-    while ((period = RE_LABEL_LINK.exec((head as RegExpExecArray).toString())) != null) {
-        result += period;
+        head: string = '',
+        body: string = '',
+        result: string = '',
+        scriptsArray: Array<string> = [];
+    while ((period = RE_LABEL_LINK.exec(html)) != null) {
+        head += period[1].trim();
     }
-    while ((period = RE_LABEL_META.exec((head as RegExpExecArray).toString())) != null) {
-        result += period;
+    while ((period = RE_LABEL_META.exec(html)) != null) {
+        head += period[1].trim();
     }
-    while ((period = RE_LABEL_STYLE.exec((head as RegExpExecArray).toString())) != null) {
-        result += period;
+    while ((period = RE_LABEL_STYLE.exec(html)) != null) {
+        head += period[1].trim();
     }
-    while ((period = RE_LABEL_SCRIPT.exec((head as RegExpExecArray).toString())) != null) {
-        result += period;
+    while ((period = RE_LABEL_SCRIPT_URL.exec(html)) != null) {
+        let urlArray = [];
+        urlArray.push(period[1].trim() + window.location.origin);
+        urlArray.forEach(async item => {
+            scriptsArray.push(await requestScript(item));
+        })
     }
-    result += (body as RegExpExecArray).toString();
-    return result;
+    while ((period = RE_LABEL_SCRIPT_CONTENT.exec(html)) != null) {
+        scriptsArray.push(period[1].trim());
+    }
+    html = html.replace(RE_LABEL_SCRIPT, `<!-- this script is replaced and run in sandbox-->`);
+    body = (RE_LABEL_BODY.exec(html) as RegExpExecArray).toString();
+    result = head + body;
+    return [result, scriptsArray];
 }
-//事实上，因为微前端的子应用都是SPA，我们可以直接将scriptfetch下来，反正后面提取钩子函数也需要。head标签中的内容就正则提取再append吧。
-export async function loadHtml(ct: string, url: string) {
+export async function requestScript(url: string): Promise<string> {
+    return await request(url);
+}
+export async function loadHtml(ct: string, url: string, appName: string) {
     let getHtml = memorize(reHtml),
-        result = await getHtml(url),
+        [result, scriptsArray] = await getHtml(url),
         container = document.getElementById(ct);
     if (!container) { throw Error('the div tag with id' + ct + 'does not exist!') } else {
         container.innerHTML = result;
+        scriptsArray.map((item: string) => { sandbox(item, appName); })
     }
 }
 
--- src/htmlLoader/regex.tsx
+++ src/htmlLoader/regex.tsx
@@ -1,8 +1,10 @@
 //const RE_STYLE_CONTENT = `/(?<=<style.*>)([\s\S]+)(?=<\/style>)/g`;/匹配<style></style>之间的内容.
-export const RE_LABEL_STYLE = /<\s*style([\s\S]+)>([^>]*)<\s*\/style>/g;
-export const RE_LABEL_SCRIPT = /<\s*script([\s\S]+)>([^>]*)<\s*\/script>/g;
-export const RE_LABEL_LINK = /<\s*link([\s\S]+)>/g;
-export const RE_LABEL_META = /<\s*meta([\s\S]+)>/g;
-export const RE_LABEL_BODY = /<\s*body([\s\S]+)>([^>]*)<\s*\/body>/g;
-export const RE_LABEL_HEAD = /<\s*head([\s\S]+)>([^>]*)<\s*\/head>/g;
+export const RE_LABEL_STYLE = new RegExp(/<\s*style([\s\S]+)>([^>]*)<\s*\/style>/g);
+export const RE_LABEL_SCRIPT_CONTENT = new RegExp(/<\s*script([\s\S]+)>([^>]*)<\s*\/script>/g);
+export const RE_LABEL_LINK = new RegExp(/<\s*link([\s\S]+)>/g);
+export const RE_LABEL_META = new RegExp(/<\s*meta([\s\S]+)>/g);
+export const RE_LABEL_BODY = new RegExp(/<\s*body([\s\S]+)>([^>]*)<\s*\/body>/g);
+export const RE_LABEL_HEAD = new RegExp(/<\s*head([\s\S]+)>([^>]*)<\s*\/head>/g);
+export const RE_LABEL_SCRIPT_URL = new RegExp(/<\s*script([\s\S]+)>([^>]*)<\s*\/script>/g);
+export const RE_LABEL_SCRIPT = new RegExp(/<\s*script([\s\S]+)>([^>]*)<\s*\/script>/g);
 
