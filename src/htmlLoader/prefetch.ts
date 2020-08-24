dde3783e37af9c9fdda1602eab9e4c6eb19c2f03
futengda <fxxjdedd@gmail.com>
2020-08-24 18:44:52 +0800

    feat: 切片 & 链式调用

--- src/htmlLoader/prefetch.ts
+++ src/htmlLoader/prefetch.ts
@@ -1,14 +1,26 @@
 import { parseHtml } from "./htmlLoader"
+
 export async function prefetch() {
-    new Promise(reslove => {
-        window.appList.forEach(async app => {
-            if (app.matchRouter) {
-                await (await parseHtml)(app.entry, app.name);
-            }
-        })
-    })
+    const appPieces = getPieces(window.appList, 3)
+    await appPieces.reduce(async (p, pieces) => {
+        await p
+        return Promise.all(
+            pieces.map(async app => (await parseHtml)(app.entry, app.name))
+        )
+    }, Promise.resolve())
 }
 
-//对 appList进行切片然后用promise.all链式调用
+function getPieces(arr, count) {
+    if (count < 1) throw 'count must greatter than 0'
+  
+    const result = Array(Math.ceil(arr.length / count)).fill([]).map(v => v.slice())
+    for (let i = 0, j = 0; i < arr.length; i++) {
+        if (result[j].length === count) j += 1
+        result[j].push(arr[i])
+    }
 
-//函数节流，主要是针对应用切换那里，应该有函数节流，避免出现莫名奇妙的情况。
+    return result
+}
+
+// TODO: 已经加载的App不要prefetch
+// TODO: 函数节流，主要是针对应用切换那里，应该有函数节流，避免出现莫名奇妙的情况。
